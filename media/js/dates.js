47cb2c99db0f2af5089a4cd7a255121be1ea555d
Radu Fericean <radu@fericean.ro>
2011-07-18 14:12:24 +0300

    updated dates, not final

--- media/js/dates.js
+++ media/js/dates.js
@@ -1,13 +1,13 @@
 $(function(){
     $("#id_arrival_date").datepicker({
         dateFormat: 'yy-mm-dd',
-        minDate: new Date(2010, 08, 10),
-        maxDate: new Date(2010, 08, 20)
+        minDate: new Date(2011, 08, 10),
+        maxDate: new Date(2011, 08, 20)
     });
     $("#id_departure_date").datepicker({
         dateFormat: 'yy-mm-dd',
-        minDate: new Date(2010, 08, 10),
-        maxDate: new Date(2010, 08, 20)
+        minDate: new Date(2011, 08, 10),
+        maxDate: new Date(2011, 08, 20)
     });
     /*if($("#id_departure_date").datepicker( "getDate" )) {
         $('#id_arrival_date').datepicker('option', 'maxDate', $("#id_departure_date").datepicker('getDate'));
