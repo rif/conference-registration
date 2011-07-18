515b8a55ef51cc81e14f0a34de2752f910a75ae4
Radu Ioan Fericean <radu@fericean.ro>
2011-07-18 12:11:17 +0300

    cleanups

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -24,26 +24,3 @@ class StatusForm(forms.ModelForm):
         model = Status
         exclude = ('user', 'payment_received')
 
-class StatisticsForm(forms.Form):
-    icsm = forms.BooleanField(required=False)
-    promise = forms.BooleanField(required=False)
-    wse = forms.BooleanField(required=False)
-    scam = forms.BooleanField(required=False)
-    famoosr = forms.BooleanField(required=False)
-    mesoa = forms.BooleanField(required=False)
-    tut1 = forms.BooleanField(required=False, help_text='Refactoring for parallelism')
-    tut2 = forms.BooleanField(required=False, help_text='Migrating software testing to the cloud')
-    tut3 = forms.BooleanField(required=False, help_text='Teaching undergraduate software engineering')
-    tut4 = forms.BooleanField(required=False, help_text='The licensing challenge')
-    extra_ICSM_banquet = forms.BooleanField(required=False)
-    extra_ICSM_reception = forms.BooleanField(required=False)
-    extra_ICSM_winetasting = forms.BooleanField(required=False)
-    extra_ICSM_proceedings = forms.BooleanField(required=False)
-    extra_PROMISE_SCAM_dinner = forms.BooleanField(required=False)
-    extra_PROMISE_proceedings = forms.BooleanField(required=False)
-    extra_SCAM_proceedings = forms.BooleanField(required=False)
-    extra_WSE_dinner_price = forms.BooleanField(required=False)
-    extra_WSE_proceedings = forms.BooleanField(required=False)
-
-class NamesForm(forms.Form):
-  names = forms.CharField(max_length=300, required=False, help_text='Comma separated names or part of the names')
