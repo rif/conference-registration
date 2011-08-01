bfcb12c954ddd55a6d210cc0e6c433398a4f544b
Radu Ioan Fericean <rif@love.ro>
2011-08-01 13:48:56 +0300

    processing gif

--- media/js/shop.js
+++ media/js/shop.js
@@ -1,9 +1,9 @@
 $(function(){
    $("#email").click(function(e){
+       $("#email-response").fadeIn();
        var email_href = $(this).attr("href");
        $.get(email_href, function(response){
             $("#email-response").text(response);
-            $("#email-response").fadeIn("slow");
         });
         e.preventDefault();
     });
