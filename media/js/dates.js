49645360ff0e899e7721ce8f9b2001d739441421
Radu Ioan Fericean <radu@fericean.ro>
2011-08-01 19:31:04 +0300

    arryval/departure dates

--- media/js/dates.js
+++ media/js/dates.js
@@ -1,13 +1,13 @@
 $(function(){
     $("#id_arrival_date").datepicker({
         dateFormat: 'yy-mm-dd',
-        minDate: new Date(2011, 08, 10),
-        maxDate: new Date(2011, 08, 20)
+        minDate: new Date(2011, 08, 24),
+        maxDate: new Date(2011, 08, 27)
     });
     $("#id_departure_date").datepicker({
         dateFormat: 'yy-mm-dd',
-        minDate: new Date(2011, 08, 10),
-        maxDate: new Date(2011, 08, 20)
+        minDate: new Date(2011, 08, 27),
+        maxDate: new Date(2011, 08, 29)
     });
     /*if($("#id_departure_date").datepicker( "getDate" )) {
         $('#id_arrival_date').datepicker('option', 'maxDate', $("#id_departure_date").datepicker('getDate'));
