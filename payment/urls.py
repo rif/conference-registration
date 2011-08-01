25057e0791fdf6925dd122858d111fca7fb5b35b
Radu Ioan Fericean <rif@love.ro>
2011-08-01 13:35:21 +0300

    email response and updated links for crisis

--- payment/urls.py
+++ payment/urls.py
@@ -4,7 +4,6 @@ from payment import views
 
 urlpatterns = patterns('',
     url(r'^shop/$', views.shop, name="shop"),
-    url(r'^paypal/$', views.paypal, name="paypal"),
     url(r'^email/$', views.email, name="email"),
     url(r'^receipts/$', views.receipts, name="receipts"),
 )
