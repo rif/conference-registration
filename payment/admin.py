f6f7feaab284e72a03fadb2a508580afb1671bf6
Radu Ioan Fericean <radu@fericean.ro>
2011-08-02 10:12:58 +0300

    mark as paid

--- payment/admin.py
+++ payment/admin.py
@@ -6,9 +6,5 @@ class PaymentPackageAdmin(admin.ModelAdmin):
     list_display = ('code', 'conference_price', 'early')
     search_fields = ['code']
 
-def mark_paid(modeladmin, request, queryset):
-    queryset.update(payment_received=True)
-mark_paid.short_description = "Confirm payment for selected items"
-
 admin.site.register(PaymentPackage, PaymentPackageAdmin)
 
