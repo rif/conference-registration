f50effe9365c247c45bc1196721172d268b3887f
Radu Ioan Fericean <rif@love.ro>
2011-08-02 10:12:58 +0300

    mark as paid

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -11,12 +11,15 @@ class AccomodationAdmin(admin.ModelAdmin):
     list_display = ('get_user_name', 'arrival_date', 'departure_date')
     search_fields = ['user__first_name', 'user__last_name']
 
+def mark_paid(modeladmin, request, queryset):
+    queryset.update(payment_received=True)
+mark_paid.short_description = "Confirm payment for selected items"
 
 class StatusAdmin(admin.ModelAdmin):
     search_fields = ['user__first_name', 'user__last_name']
-    list_display = ('__unicode__', 'IEEE_member_number', 'student')
+    list_display = ('__unicode__', 'IEEE_member_number', 'student', 'payment_received')
     list_filter = ('student',)
-
+    actions = [mark_paid]
 
 admin.site.register(About, AboutAdmin)
 admin.site.register(Accomodation, AccomodationAdmin)
