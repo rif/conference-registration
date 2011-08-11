204da759381025a7b7c5ea631605741d065e411d
Radu Ioan Fericean <radu@fericean.ro>
2011-08-11 11:52:12 +0300

    changed camera ready date

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,33 @@
-from fabric.api import hosts, prompt, sudo, run, local, cd
+from fabric.api import sudo, prompt, run, local, cd
+from fabric.decorators import runs_once, hosts, task
+from fabric.colors import green
 
+@task
 def ci():
-    """Commit localy using mercurial"""
+    'Commit localy using mercurial'
     comment = prompt('Commit comment: ', default='another commit from fabric')
     local('hg ci -m "%s"' % comment)
+    push()
+
+@runs_once
+def push():
+    print(green('pushing...'))
     local('hg push')
 
-"""
-For running sudo on remote machine:
-    vi /etc/sudoers (EDIT: please use visudo instead)
-    comment out: #Default requiretty
-"""
-@hosts('horia@loose.upt.ro:22022')
+@task
+@hosts('rif@loose')
 def deploy():
+    'triggers hg pul on the server'
+    print(green('deploying...'))
+    push()
     'Deploy the app to the target environment'
-    local('hg push ssh://loose/../icsmreg/icsm_registration')
-    with cd('/home/icsmreg/icsm_registration'):
-        run('hg up')
+    with cd('conference_registration'):
+        run('hg pul -uv')
 
-@hosts('horia@loose.upt.ro:22022')
+
+@task
+@hosts('loose')
 def reload():
     'fires an apache graceful reload'
-    sudo('apache2ctl graceful')
+    print(green('reloading...'))
+    sudo('/etc/init.d/apache2 reload')
